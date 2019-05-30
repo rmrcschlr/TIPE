84d38e350118422045045c377cfcfdaf47f36c6b
yjourdin <45071608+yjourdin@users.noreply.github.com>
2019-05-30 20:18:13 +0200

    Update and rename class_frequences.py to class_cadencements.py

--- class_cadencements.py
+++ class_cadencements.py
@@ -1,18 +1,29 @@
-class Frequences :
+def supprime_doublons(liste) :
+    liste_sans_doublons = [liste[0]]
+    for loop1 in range(len(liste)) :
+        condition = True
+        for loop2 in liste_sans_doublons :
+            if liste[loop1] == loop2 :
+                condition = False
+        if condition :
+            liste_sans_doublons.append(liste[loop1])
+    return liste_sans_doublons
+
+class Cadencements :
     
     def __init__(self, reseau) :
         self.liste = []
         for i in range(len(reseau.liste_lignes)) :
             self.liste.append([reseau.liste_lignes[i][0]])
-    # Frequences -> Reseau -> Frequences
+    # Cadencements -> Reseau -> unit
     
     def ajouter_ligne(self, nom) :
         self.liste.append([nom])
-    # Frequences -> str -> unit
+    # Cadencements -> string -> unit
     
     def ajouter_frequence(self, indice_ligne, debut, fin, freq) :
         self.liste[indice_ligne].append([debut, fin, freq])
-    # Frequences -> int -> int -> int -> int -> unit
+    # Cadencements -> int -> int -> int -> int -> unit
     
     def indice_ligne(self, nom) :
         indice = 0
@@ -21,17 +32,17 @@ class Frequences :
         if indice == len(self.liste) :
             return -1
         return indice
-    # Frequences -> str -> int
+    # Cadencements -> string -> int
     
     def nom_ligne(self, indice) :
         return self.liste[indice][0]
-    # Frequences -> int -> str
+    # Cadencements -> int -> string
     
     def freq_ligne(self, indice_ligne) :
         return self.liste[indice_ligne][1:]
-    # Frequences -> int -> list of [int, int, int]
+    # Cadencements -> int -> list of [int, int, int]
     
-    def trier_ligne(self,indice_ligne) :
+    def trier_ligne(self, indice_ligne) :
         self.liste[indice_ligne][1:].sort()
         i = 2
         while i < len(self.liste[indice_ligne]) :
@@ -39,12 +50,12 @@ class Frequences :
                 self.liste[indice_ligne].pop(i)
                 i -= 1
             i += 1
-    # Frequences -> int -> unit
+    # Cadencements -> int -> unit
     
     def trier(self) :
         for i in range(len(self.liste)) :
             self.trier_ligne(i)
-    # Frequences -> unit
+    # Cadencements -> unit
     
     def normaliser(self) :
         self.trier()
@@ -53,7 +64,7 @@ class Frequences :
                 if self.freq_ligne(i)[j][1] >= self.freq_ligne(i)[j + 1][0] :
                     moyenne = (self.freq_ligne(i)[j][1] + self.freq_ligne(i)[j + 1][0]) // 2
                     self.liste[i][j + 1][1], self.liste[i][j + 2][0] = moyenne, moyenne + 1
-    # Frequences -> unit
+    # Cadencements -> unit
     
     def prochain_depart(self, indice_ligne, depart_precedent) :
         self.normaliser()
@@ -63,6 +74,6 @@ class Frequences :
         ecart = self.freq_ligne(indice_ligne)[tranche][2]
         if depart_precedent + ecart > self.freq_ligne(indice_ligne)[-1][1] :
             return [-1, -1]
-        intervalle = (self.freq_ligne(indice_ligne)[tranche][2] // 10) + 1
+        intervalle = (ecart // 10) + 1
         return [ecart, intervalle]
-    # Frequences -> int -> int -> [int, int]
+    # Cadencements -> int -> int -> [int, int]
