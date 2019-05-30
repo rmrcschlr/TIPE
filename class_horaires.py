14aeb51f93fa7307c63438c6653c85a9ea5c5063
yjourdin <45071608+yjourdin@users.noreply.github.com>
2019-05-30 20:18:41 +0200

    Update class_horaires.py

--- class_horaires.py
+++ class_horaires.py
@@ -1,4 +1,4 @@
-def indexs(valeur, liste) :
+def liste_places(valeur, liste) :
     places = []
     for i in range(len(liste)) :
         if liste[i] == valeur :
@@ -7,58 +7,47 @@ def indexs(valeur, liste) :
 
 class Horaires :
     
-    def __init__(self, reseau, mega_matrice) :
-        self.reseau = reseau
+    def __init__(self, departs_terminus) :
+        self.departs_terminus = departs_terminus
         self.liste = []
-        for indice_ligne in range(len(reseau.liste_lignes)) :
+        for indice_ligne in range(len(RESEAU.liste_lignes)) :
             ligne = []
-            for indice_arret in reseau.ligne_arrets(indice_ligne) :
+            for indice_arret in RESEAU.ligne_arrets(indice_ligne) :
                 arret = array([])
-                if indice_arret in reseau.ligne_terminus(indice_ligne) :
-                    arret = array(mega_matrice[indice_ligne][reseau.ligne_terminus(indice_ligne).index(indice_arret)][:])
+                if indice_arret in RESEAU.ligne_terminus(indice_ligne) :
+                    arret = array(departs_terminus[indice_ligne][RESEAU.ligne_terminus(indice_ligne).index(indice_arret)][:])
                 ligne.append(arret)
             self.liste.append(ligne)
-        #self.remplir_horaires(reseau)
-    # Horaires -> Reseau -> list of list of list of int -> Horaires
+        self.remplir_horaires()
+    # Horaires -> int list list list -> unit
     
-    def remplir_horaires(self, reseau) :
+    def remplir_horaires(self) :
         for indice_ligne in range(len(self.liste)) :
             for indice_arret in range(len(self.liste[indice_ligne])) :
                 if array_equal(self.liste[indice_ligne][indice_arret], []) :
                     compteur = 0
                     trajet = 0
                     while array_equal(self.liste[indice_ligne][indice_arret - compteur], []) :
-                        trajet += self.reseau.matrice_segments[self.reseau.ligne_arrets(indice_ligne)[indice_arret - compteur - 1]][self.reseau.ligne_arrets(indice_ligne)[indice_arret - compteur]]
+                        trajet += RESEAU.matrice_segments[RESEAU.ligne_arrets(indice_ligne)[indice_arret - compteur - 1]][RESEAU.ligne_arrets(indice_ligne)[indice_arret - compteur]]
                         compteur += 1
                     self.liste[indice_ligne][indice_arret] = self.liste[indice_ligne][indice_arret - compteur] + trajet
     # Horaires -> unit
     
     def horaires_ligne(self, indice_ligne) :
         return self.liste[indice_ligne]
-    # Horaires -> int -> list of array of int
+    # Horaires -> int -> int array list
     
     def horaires_arret(self, indice_arret) :
         liste = []
         for indice_ligne in range(len(self.liste)) :
-            for indice in indexs(indice_arret, self.reseau.ligne_arrets(indice_ligne)) :
+            for indice in liste_places(indice_arret, RESEAU.ligne_arrets(indice_ligne)) :
                 indice_suivant = indice + 1
-                if indice_suivant == len(self.reseau.ligne_arrets(indice_ligne)) :
+                if indice_suivant == len(RESEAU.ligne_arrets(indice_ligne)) :
                     indice_suivant = 0
-                temp = (self.reseau.ligne_arrets(indice_ligne)[indice_suivant], self.liste[indice_ligne][indice]) # le premier element est la direction du bus
+                temp = (RESEAU.ligne_arrets(indice_ligne)[indice_suivant], self.liste[indice_ligne][indice]) # le premier element est la direction du bus
                 liste.append(temp)
         return liste
-    # Horaires -> int -> list of (int, array of int)
-    
-    def horaires_arret_vers_arret(self, indice_arret_init, indice_arret_final) :
-        liste = self.horaires_arret(indice_arret_init)
-        compteur = 0
-        while liste[compteur][0] != indice_arret_final and compteur < len(liste) :
-            compteur += 1
-        if compteur != len(liste) :
-            result = liste[compteur][1]
-        else :
-            result = []
-        return result
+    # Horaires -> int -> (int, int array) list
     
     def horaire_suivant(self, indice_arret, heure) :
         result = []
@@ -66,24 +55,10 @@ class Horaires :
         for indice in range(len(liste)) :
             compteur = 0
             horaire = liste[indice][1][compteur]
-            while horaire < heure and compteur < len(liste[indice][1]) :
+            while horaire < heure and compteur < len(liste[indice][1]) - 1 :
                 compteur += 1
                 horaire = liste[indice][1][compteur]
             if compteur != len(liste[indice][1]) :
                 result.append((liste[indice][0], horaire))
         return result
-    # Horaires -> int -> int -> list of (int, int)
-    
-    def horaire_suivant_vers_arret(self, indice_arret_init, indice_arret_final, heure) :
-        liste = self.horaires_arret_vers_arret(indice_arret_init, indice_arret_final)
-        compteur = 0
-        horaire = liste[compteur]
-        while horaire < heure and compteur < len(liste) :
-            compteur += 1
-            horaire = liste[compteur]
-        if compteur != len(liste) :
-            result = horaire
-        else :
-            result = -1
-        return result
-    
+    # Horaires -> int -> int -> (int, int) list
