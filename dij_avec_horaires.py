006c9d41166ccaa4daa14e3395aebbb514a73f8f
yjourdin <45071608+yjourdin@users.noreply.github.com>
2019-05-30 20:20:13 +0200

    Update dij_avec_horaires.py

--- dij_avec_horaires.py
+++ dij_avec_horaires.py
@@ -4,16 +4,14 @@ def voisin(M,t,interdit):
         if M[t][i] != -1 and not(i in interdit):
             voisin.append(i)
     return voisin
-    
-# list of list of float -> int -> list of int -> list of int
+# int list list-> int -> int list-> int list
 
 def colonne(mat,c):
     L=[]
     for i in range(len(mat)):
         L.append(mat[i][c])
     return L
-    
-# list of list of a' -> int -> list of a'
+# a' list list-> int -> a' list
 
 def sans_0(K):
     k=[]
@@ -21,19 +19,24 @@ def sans_0(K):
         if i != 0:
             k.append(i)
     return k
-
-# list of float -> list of float
+# int list-> int list
 
 def Dijkstra_adapte(matrice, horaires, arret_init, arret_final, heure_depart) :
+    # liste : [horaire à l'arret, chemin] pour tous les arrets
     liste = [ [0,[]] for i in range(len(matrice)) ]
+    # Initialise la liste
     liste[arret_init][0] = heure_depart
     liste[arret_init][1] = [arret_init]
+    # arretsInterdits : liste des arrets déjà passés
     arrets_interdits = []
     arret_actuel = arret_init
+    # Parcourir jusqu'à l'arret final
     while arret_actuel != arret_final :
         arrets_interdits.append(arret_actuel)
         liste_voisins = voisin(matrice, arret_actuel, arrets_interdits)
+        # 
         horaires_arret_actuel = horaires.horaire_suivant(arret_actuel, liste[arret_actuel][0])
+        # Pour tous les voisins
         for arret_voisin in liste_voisins :
             indice = 0
             while horaires_arret_actuel[indice][0] != arret_voisin :
@@ -51,5 +54,4 @@ def Dijkstra_adapte(matrice, horaires, arret_init, arret_final, heure_depart) :
         liste[arret_plus_proche][1] += [arret_plus_proche]
         arret_actuel = arret_plus_proche
     return liste[arret_final]
-    
-# list of list of float -> int -> int -> float*list of int
+# int list list -> Horaires -> int -> int -> int -> (int, int list)
