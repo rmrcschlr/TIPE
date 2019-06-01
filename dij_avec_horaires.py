e0c960515cb24ae21826e6e2e0fa41ca3a37446c
yjourdin <45071608+yjourdin@users.noreply.github.com>
2019-06-01 15:22:16 +0200

    Update dij_avec_horaires.py

--- dij_avec_horaires.py
+++ dij_avec_horaires.py
@@ -1,57 +1,49 @@
-def voisin(M,t,interdit):
+from numpy import inf
+
+def sans(liste, valeur) :
+    result = []
+    for element in liste :
+        if element != valeur :
+            result.append(element)
+    return result
+
+def voisin(matrice, arret, liste):
     voisin=[]
-    for i in range(len(M[t])):
-        if M[t][i] != -1 and not(i in interdit):
+    for i in sans(liste, arret) :
+        if matrice[arret][i] != inf :
             voisin.append(i)
     return voisin
-# int list list-> int -> int list-> int list
+# int list list-> int -> int list -> int list
 
-def colonne(mat,c):
-    L=[]
-    for i in range(len(mat)):
-        L.append(mat[i][c])
-    return L
-# a' list list-> int -> a' list
+def indice_minimum(liste, liste_indices) :
+    indice_min = liste_indices[0]
+    min = liste[liste_indices[0]]
+    for indice in liste_indices[1:] :
+        if liste[indice] < min :
+            indice_min = indice
+            min = liste[indice]
+    return indice_min
+# int list -> int list -> int 
 
-def sans_0(K):
-    k=[]
-    for i in K:
-        if i != 0:
-            k.append(i)
-    return k
-# int list-> int list
-
-def Dijkstra_adapte(matrice, horaires, arret_init, arret_final, heure_depart) :
-    # liste : [horaire à l'arret, chemin] pour tous les arrets
-    liste = [ [0,[]] for i in range(len(matrice)) ]
-    # Initialise la liste
-    liste[arret_init][0] = heure_depart
-    liste[arret_init][1] = [arret_init]
-    # arretsInterdits : liste des arrets déjà passés
-    arrets_interdits = []
+def Dijkstra_adapte(matrice, horaires, arret_init, heure_depart) :
+    heures = [inf] * len(matrice)
+    heures[arret_init] = heure_depart
+    predecesseurs = [i for i in range(len(matrice))]
+    arrets_non_passes = sans([i for i in range(len(matrice))], arret_init)
     arret_actuel = arret_init
-    # Parcourir jusqu'à l'arret final
-    while arret_actuel != arret_final :
-        arrets_interdits.append(arret_actuel)
-        liste_voisins = voisin(matrice, arret_actuel, arrets_interdits)
-        # 
-        horaires_arret_actuel = horaires.horaire_suivant(arret_actuel, liste[arret_actuel][0])
-        # Pour tous les voisins
+    while arrets_non_passes != [] :
+        liste_voisins = voisin(matrice, arret_actuel, arrets_non_passes)
+        horaires_arret_actuel = horaires.horaire_suivant(arret_actuel, heures[arret_actuel])
         for arret_voisin in liste_voisins :
             indice = 0
             while horaires_arret_actuel[indice][0] != arret_voisin :
                 indice += 1
             horaire_vers_voisin = horaires_arret_actuel[indice][1]
-            if liste[arret_voisin][0] == 0 :
-                liste[arret_voisin][0] = horaire_vers_voisin + matrice[arret_actuel][arret_voisin]
-                liste[arret_voisin][1] = liste[arret_actuel][1][:]
-            else:
-                if horaire_vers_voisin + matrice[arret_actuel][arret_voisin] < liste[arret_voisin][0]:
-                    liste[arret_voisin][1] = liste[arret_actuel][1][:]
-                liste[arret_voisin][0] = min(horaire_vers_voisin + matrice[arret_actuel][arret_voisin], liste[arret_voisin][0])
-        liste[arret_actuel][0] = 0
-        arret_plus_proche = colonne(liste,0).index(min(sans_0(colonne(liste, 0))))
-        liste[arret_plus_proche][1] += [arret_plus_proche]
+            if heures[arret_voisin] > horaire_vers_voisin + matrice[arret_actuel][arret_voisin] :
+                heures[arret_voisin] = horaire_vers_voisin + matrice[arret_actuel][arret_voisin]
+                predecesseurs[arret_voisin] = arret_actuel
+        arret_plus_proche = indice_minimum(heures, arrets_non_passes)
         arret_actuel = arret_plus_proche
-    return liste[arret_final]
-# int list list -> Horaires -> int -> int -> int -> (int, int list)
+        arrets_non_passes.remove(arret_plus_proche)
+    return heures
+# int list list -> Horaires -> int -> int -> int -> int list
